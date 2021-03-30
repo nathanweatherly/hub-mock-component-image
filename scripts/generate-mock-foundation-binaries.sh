797d97e914a3dbc011ddc985fb83c55f7aea99c0
Nathan Weatherly <nweather@redhat.com>
2021-03-30 14:16:01 -0400

    I THINK IT iS WORKING

--- scripts/generate-mock-foundation-binaries.sh
+++ scripts/generate-mock-foundation-binaries.sh
@@ -1,5 +1,7 @@
 GOOS=linux go build ./templates/hello-world.go
 cp hello-world ./bin/controller
-cp hello-world ./bin/webhook
+# cp hello-world ./bin/webhook
 rm hello-world
+GOOS=linux go build ./templates/mutating-webhook.go
+mv mutating-webhook ./bin/webhook
 cp ./templates/template-proxyserver-binary ./bin/proxyserver
--- templates/hello-world.go
+++ templates/hello-world.go
@@ -15,11 +15,6 @@ func main() {
 	http.HandleFunc("/healthz", Ping)
 	http.HandleFunc("/readyz", Ping)
 	http.ListenAndServe(":8000", nil)
-	// http.ListenAndServe(":6443", nil) // This doesn't work; proxy server has HTTPS
-	// err := http.ListenAndServeTLS(":6443", "", "", nil)
-	// if err != nil {
-	// 	log.Fatal("ListenAndServeTLS: ", err)
-	// }
 	for {
 		time.Sleep(5 * time.Second)
 		fmt.Println("Still Running...")
