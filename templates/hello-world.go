d7b0f4da6e562182e54c524eecdfb1048823994f
Zhiwei Yin <zyin@redhat.com>
2021-03-30 19:26:55 +0800

    a mock webhook example

--- templates/hello-world.go
+++ templates/hello-world.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"crypto/tls"
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	v1 "k8s.io/kubernetes/staging/src/k8s.io/api/admission/v1"
 	"net/http"
 	"time"
 )
@@ -23,5 +27,72 @@ func main() {
 	for {
 		time.Sleep(5 * time.Second)
 		fmt.Println("Still Running...")
+	}
+}
+
+func webhookRun() error {
+	http.HandleFunc("/mutating", webhookServer)
+
+	server := &http.Server{
+		Addr:      ":8000",
+		TLSConfig: configTLS(),
+	}
+	err := server.ListenAndServeTLS("", "")
+	if err != nil {
+		fmt.Errorf("Listen server tls error: %+v", err)
+		return err
+	}
+
+	return nil
+}
+
+func webhookServer(w http.ResponseWriter, r *http.Request){
+	var body []byte
+	if r.Body != nil {
+		if data, err := ioutil.ReadAll(r.Body); err == nil {
+			body = data
+		}
+	}
+
+	// The AdmissionReview that was sent to the webhook
+	requestedAdmissionReview := v1.AdmissionReview{}
+
+	// The AdmissionReview that will be returned
+	responseAdmissionReview := v1.AdmissionReview{
+		Response: &v1.AdmissionResponse{Allowed: true},
+	}
+
+	if err := json.Unmarshal(body, &requestedAdmissionReview); err != nil {
+		fmt.Errorf("error %v",err)
+		return
+	}
+
+	responseAdmissionReview.Kind = requestedAdmissionReview.Kind
+	responseAdmissionReview.APIVersion = requestedAdmissionReview.APIVersion
+	// Return the same UID
+	responseAdmissionReview.Response.UID = requestedAdmissionReview.Request.UID
+
+	respBytes, err := json.Marshal(responseAdmissionReview)
+	if err != nil {
+		fmt.Errorf("error %v",err)
+		return
+	}
+	if _, err := w.Write(respBytes); err != nil {
+		fmt.Errorf("error %v",err)
+		return
+	}
+}
+
+func configTLS() *tls.Config {
+	certFile := "/var/run/ocm-webhook/tls.crt"
+	keyFile := "/var/run/ocm-webhook/tls.key"
+	sCert, err := tls.LoadX509KeyPair(certFile, keyFile)
+	if err != nil {
+		fmt.Errorf("error %v",err)
+	}
+
+	return &tls.Config{
+		MinVersion:   tls.VersionTLS12,
+		Certificates: []tls.Certificate{sCert},
 	}
 }
